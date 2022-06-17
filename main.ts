d002af5648b82a9c785b2ad45f6d86054846f292
yenci84 <107704483+yenci84@users.noreply.github.com>
2022-06-17 11:05:34 -0500

    Archivos iniciales para el proyecto MakeCode

---
+++ main.ts
@@ -0,0 +1,12 @@
+let Dado = 0
+input.onGesture(Gesture.Shake, function () {
+    let Número = 0
+    soundExpression.giggle.play()
+    basic.pause(300)
+    Dado = randint(1, 6)
+    if (Número) {
+    	
+    } else {
+    	
+    }
+})
